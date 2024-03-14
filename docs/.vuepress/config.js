38d46f5360fc56bd9b1afc4214a180784d7faaa1
李振宇 <lizhenyu@joyware.com>
2024-03-14 17:02:13 +0800

    更新git 的 hash，删除过时的VueCLI

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,146 +1,142 @@
 const { removePlugin, PLUGINS } = require("@vuepress/markdown");
 module.exports = {
-	title: "PlumNotes",
-	description: "李振宇的个人笔记",
-	base: "/guideNotes/",
-	head: [["link", { rel: "icon", href: "/icon.png" }]],
-	port: 3000,
-	markdown: {
-		lineNumbers: false,
-		chainMarkdown(config) {
-			removePlugin(config, PLUGINS.EMOJI);
-		},
-	},
-	themeConfig: {
-		lastUpdated: "最后更新时间",
-		sidebar: "auto",
-		repo: "https://github.com/happyplum/guideNotes",
-		repoLabel: "Github",
-		nav: [
-			{
-				text: "前端开发笔记",
-				items: [
-					{
-						text: "JavaScript",
-						link: "/javascript/",
-					},
-					{
-						text: "TypeScript",
-						items: [
-							{ text: "TypeScript", link: "/typescript/typescript/" },
-							{ text: "ts.config", link: "/typescript/tsconfig/" },
-						],
-					},
-				],
-			},
-			{
-				text: "前端开发笔记2",
-				items: [
-					{
-						text: "JSDoc",
-						link: "/jsdoc/",
-					},
-					{
-						text: "Vue",
-						items: [
-							{
-								text: "Vue2",
-								link: "/vue/vue2/",
-							},
-							{
-								text: "Vue3",
-								link: "/vue/vue3/",
-							},
-						],
-					},
-					{
-						text: "React",
-						items: [
-							{
-								text: "ReactNative",
-								link: "/react/reactnative/",
-							},
-						],
-					},
-				],
-			},
-			{
-				text: "脚手架",
-				items: [
-					{
-						text: "VueCLI",
-						link: "/vuecli/vuecli/",
-					},
-					{
-						text: "Vite",
-						items: [
-							{ text: "cli迁移", link: "/vuecli/vite/cli2vite/" },
-							{
-								text: "vit.config笔记",
-								link: "/vuecli/vite/config/",
-							},
-						],
-					},
-					{
-						text: "工具",
-						items: [{ text: "自定义工具", link: "/tools/cli/" }],
-					},
-				],
-			},
-			{
-				text: "工具",
-				items: [
-					{
-						text: "打包相关",
-						items: [
-							{
-								text: "Rollup",
-								link: "/builde/rollup/",
-							},
-							{
-								text: "Webpack",
-								link: "/builde/webpack/",
-							},
-						],
-					},
-					{
-						text: "git",
-						link: "/notes/git/",
-					},
-				],
-			},
-			{
-				text: "个人",
-				items: [
-					{
-						text: "nas/sPC",
-						link: "/nastools/",
-					},
-					{
-						text: "docker",
-						link: "/docker/",
-					},
-					{
-						text: "gitlab",
-						items: [
-							{ text: "gitlab", link: "/gitlab/gitlab/" },
-							{ text: "gitlabRunner", link: "/gitlab/gitlabrunner/" },
-						],
-					},
-					{
-						text: "视频编码",
-						items: [{ text: "ffmpeg", link: "/videoencodes/ffmpeg/" }],
-					},
-				],
-			},
-		],
-		sidebar: {},
-	},
-	configureWebpack: {
-		resolve: {
-			alias: {
-				"@images": "../images",
-			},
-		},
-	},
+  title: "PlumNotes",
+  description: "李振宇的个人笔记",
+  base: "/guideNotes/",
+  head: [["link", { rel: "icon", href: "/icon.png" }]],
+  port: 3000,
+  markdown: {
+    lineNumbers: false,
+    chainMarkdown(config) {
+      removePlugin(config, PLUGINS.EMOJI);
+    },
+  },
+  themeConfig: {
+    lastUpdated: "最后更新时间",
+    sidebar: "auto",
+    repo: "https://github.com/happyplum/guideNotes",
+    repoLabel: "Github",
+    nav: [
+      {
+        text: "前端开发笔记",
+        items: [
+          {
+            text: "JavaScript",
+            link: "/javascript/",
+          },
+          {
+            text: "TypeScript",
+            items: [
+              { text: "TypeScript", link: "/typescript/typescript/" },
+              { text: "ts.config", link: "/typescript/tsconfig/" },
+            ],
+          },
+        ],
+      },
+      {
+        text: "前端开发笔记2",
+        items: [
+          {
+            text: "JSDoc",
+            link: "/jsdoc/",
+          },
+          {
+            text: "Vue",
+            items: [
+              {
+                text: "Vue2",
+                link: "/vue/vue2/",
+              },
+              {
+                text: "Vue3",
+                link: "/vue/vue3/",
+              },
+            ],
+          },
+          {
+            text: "React",
+            items: [
+              {
+                text: "ReactNative",
+                link: "/react/reactnative/",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        text: "脚手架",
+        items: [
+          {
+            text: "Vite",
+            items: [
+              { text: "cli迁移", link: "/vuecli/vite/cli2vite/" },
+              {
+                text: "vit.config笔记",
+                link: "/vuecli/vite/config/",
+              },
+            ],
+          },
+          {
+            text: "工具",
+            items: [{ text: "自定义工具", link: "/tools/cli/" }],
+          },
+        ],
+      },
+      {
+        text: "工具",
+        items: [
+          {
+            text: "打包相关",
+            items: [
+              {
+                text: "Rollup",
+                link: "/builde/rollup/",
+              },
+              {
+                text: "Webpack",
+                link: "/builde/webpack/",
+              },
+            ],
+          },
+          {
+            text: "git",
+            link: "/notes/git/",
+          },
+        ],
+      },
+      {
+        text: "个人",
+        items: [
+          {
+            text: "nas/sPC",
+            link: "/nastools/",
+          },
+          {
+            text: "docker",
+            link: "/docker/",
+          },
+          {
+            text: "gitlab",
+            items: [
+              { text: "gitlab", link: "/gitlab/gitlab/" },
+              { text: "gitlabRunner", link: "/gitlab/gitlabrunner/" },
+            ],
+          },
+          {
+            text: "视频编码",
+            items: [{ text: "ffmpeg", link: "/videoencodes/ffmpeg/" }],
+          },
+        ],
+      },
+    ],
+    sidebar: {},
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@images": "../images",
+      },
+    },
+  },
 };
--- docs/tools/cli/README.md
+++ docs/tools/cli/README.md
@@ -8,12 +8,12 @@ sidebar: auto
 
 写笔记时开发的工具为 swaggerconvapi2,目前已发布到 npm,开发目的为较简单的通过 swagger 通过 config 文件配合生成 api.ts,api.d.ts,api.params.d.ts
 
--   预期牵扯到的功能为,传参,硬盘读写,打包,ts 生成,
--   后期可能考虑使用额外的指令, ts 重写,生成完毕后使用 prettier 格式化
--   打包工具选定为 rollup
--   使用的仓库管理工具为 yarn,version>2
--   使用的编写方式为 esm
--   需要有 bin 文件支持全局安装使用
+- 预期牵扯到的功能为,传参,硬盘读写,打包,ts 生成,
+- 后期可能考虑使用额外的指令, ts 重写,生成完毕后使用 prettier 格式化
+- 打包工具选定为 rollup
+- 使用的仓库管理工具为 yarn,version>2
+- 使用的编写方式为 esm
+- 需要有 bin 文件支持全局安装使用
 
 :::tip
 本配置文件包含现在用的,和下版本考虑添加的组件,可以无视
@@ -23,45 +23,45 @@ sidebar: auto
 
 ```json
 {
-	"name": "swaggerconvapi2",
-	"version": "1.0.6",
-	"description": "swaggerConvApi2 工厂化 新增配置项,配置文件 新增模板 独立函数解析封装,非开源项目,请见谅",
-	"type": "module",
-	"main": "./index.cjs", //引用时调用的入口文件,未配置bin时可安装swaggerconvapi2后npx调用
-	"bin": {
-		//执行文件,方便全局安装后调用,使用的调用指令为swapi
-		"swapi": "./bin/swapi.cjs"
-	},
-	"files": ["bin", "index.cjs"], //需要npm上传的文件,文件夹
-	"scripts": {
-		//yarn run时候的执行命令
-		"build": "rollup -c",
-		"lint": "prettier './src/**/*.{ts,js,d.ts}' -w",
-		"lint-dist": "prettier './dist/**/*.{ts,d.ts}' -w"
-	},
-	"dependencies": {
-		"prettier": "^2.6.2", //用于下版本,使用execa来调用prettier格式化方案
-		"prettier-standard": "^16.4.1" //prettier基本样式配置
-	},
-	"devDependencies": {
-		"@rollup/plugin-commonjs": "^21.1.0", //用于兼容cjs格式的组件引入
-		"@rollup/plugin-json": "^4.1.0", //用于导入json
-		"@rollup/plugin-node-resolve": "^13.2.1", //帮助rollup在打包时查找外部模块引入
-		"@types/node": "^17.0.25", //node的type声明
-		"esbuild": "^0.14.36", //用于下版本,esbuild编译工具
-		"execa": "^4.1.0", //用于下版本,执行外部指令格式化
-		"fs-extra": "^10.1.0", //用于下版本,替代fs操作的增强库
-		"minimist": "^1.2.6", //参数支持
-		"rollup": "^2.70.2", //打包工具
-		"rollup-plugin-esbuild": "^4.9.1", //用于下版本,用于支持esbuild
-		"rollup-plugin-filesize": "^9.1.2", //用于打包完毕后显示文件大小
-		"rollup-plugin-terser": "^7.0.2", //目前使用的打包工具
-		"typescript": "^4.6.3" //ts支持
-	},
-	"author": "407852978@qq.com", //邮箱
-	"license": "UNLICENSED", //私人
-	"packageManager": "yarn@3.2.0", //yarn工具
-	"prepublishOnly": "yarn run build" //publish前需要执行的步骤
+  "name": "swaggerconvapi2",
+  "version": "1.0.6",
+  "description": "swaggerConvApi2 工厂化 新增配置项,配置文件 新增模板 独立函数解析封装,非开源项目,请见谅",
+  "type": "module",
+  "main": "./index.cjs", //引用时调用的入口文件,未配置bin时可安装swaggerconvapi2后npx调用
+  "bin": {
+    //执行文件,方便全局安装后调用,使用的调用指令为swapi
+    "swapi": "./bin/swapi.cjs"
+  },
+  "files": ["bin", "index.cjs"], //需要npm上传的文件,文件夹
+  "scripts": {
+    //yarn run时候的执行命令
+    "build": "rollup -c",
+    "lint": "prettier './src/**/*.{ts,js,d.ts}' -w",
+    "lint-dist": "prettier './dist/**/*.{ts,d.ts}' -w"
+  },
+  "dependencies": {
+    "prettier": "^2.6.2", //用于下版本,使用execa来调用prettier格式化方案
+    "prettier-standard": "^16.4.1" //prettier基本样式配置
+  },
+  "devDependencies": {
+    "@rollup/plugin-commonjs": "^21.1.0", //用于兼容cjs格式的组件引入
+    "@rollup/plugin-json": "^4.1.0", //用于导入json
+    "@rollup/plugin-node-resolve": "^13.2.1", //帮助rollup在打包时查找外部模块引入
+    "@types/node": "^17.0.25", //node的type声明
+    "esbuild": "^0.14.36", //用于下版本,esbuild编译工具
+    "execa": "^4.1.0", //用于下版本,执行外部指令格式化
+    "fs-extra": "^10.1.0", //用于下版本,替代fs操作的增强库
+    "minimist": "^1.2.6", //参数支持
+    "rollup": "^2.70.2", //打包工具
+    "rollup-plugin-esbuild": "^4.9.1", //用于下版本,用于支持esbuild
+    "rollup-plugin-filesize": "^9.1.2", //用于打包完毕后显示文件大小
+    "rollup-plugin-terser": "^7.0.2", //目前使用的打包工具
+    "typescript": "^4.6.3" //ts支持
+  },
+  "author": "407852978@qq.com", //邮箱
+  "license": "UNLICENSED", //私人
+  "packageManager": "yarn@3.2.0", //yarn工具
+  "prepublishOnly": "yarn run build" //publish前需要执行的步骤
 }
 ```
 
@@ -78,28 +78,28 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 export default {
-	input: "./src/index.js", //入口我呢见
-	output: [
-		{
-			file: "index.cjs", //输出文件
-			format: "cjs", //输出格式,为了兼容性方便调用,我们输出cjs格式
-		},
-	],
-	external: ["eslint"], //打包时排除的组件
-	plugins: [
-		nodeResolve(), //该插件用于载入依赖
-		commonjs(), //载入cjs依赖
-		json(), //载入json
-		terser({
-			//打包
-			compress: {}, //压缩
-			mangle: {
-				toplevel: true, //在顶层作用域中压缩清理变量
-				eval: true, //允许使用eval
-			},
-		}),
-		filesize(), //显示下生成的输出
-	],
+  input: "./src/index.js", //入口我呢见
+  output: [
+    {
+      file: "index.cjs", //输出文件
+      format: "cjs", //输出格式,为了兼容性方便调用,我们输出cjs格式
+    },
+  ],
+  external: ["eslint"], //打包时排除的组件
+  plugins: [
+    nodeResolve(), //该插件用于载入依赖
+    commonjs(), //载入cjs依赖
+    json(), //载入json
+    terser({
+      //打包
+      compress: {}, //压缩
+      mangle: {
+        toplevel: true, //在顶层作用域中压缩清理变量
+        eval: true, //允许使用eval
+      },
+    }),
+    filesize(), //显示下生成的输出
+  ],
 };
 ```
 
@@ -115,9 +115,9 @@ bin/swapi.cjs 文件如下
 #!/usr/bin/env node
 "use strict";
 (async function main() {
-	process.exitCode = await require("../index.cjs");
+  process.exitCode = await require("../index.cjs");
 })().catch((err) => {
-	console.error(err);
+  console.error(err);
 });
 ```
 
@@ -132,3 +132,25 @@ npm 一般会需要二次验证,建议使用 authenticator 等软件绑定下
 :::waring
 发布需要检查下使用源,请使用 nrm/yrm 等工具检查自己的使用源,确保为 npm[https://registry.npmjs.org/]才能正常的发布到 npm 上
 :::
+
+## 关于增加 git 的 hash
+
+使用`execSync`指令外部执行获取 git 的 hash,并拼接到版本号中
+
+```js
+import { execSync } from "node:child_process";
+const commitDate = execSync('git log -1 --pretty=format:"%h"')
+  .toString()
+  .trimEnd();
+const branchName = execSync("git rev-parse --abbrev-ref HEAD")
+  .toString()
+  .trimEnd();
+const commitHash = execSync("git rev-parse HEAD").toString().trimEnd();
+const lastCommitMessage = execSync("git show -s --format=%s")
+  .toString()
+  .trimEnd();
+process.env.VITE_GIT_COMMIT_DATE = commitDate;
+process.env.VITE_GIT_BRANCH_NAME = branchName;
+process.env.VITE_GIT_COMMIT_HASH = commitHash;
+process.env.VITE_GIT_LAST_COMMIT_MESSAGE = lastCommitMessage;
+```
