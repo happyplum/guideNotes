cfa842e92d209b13362030636a5b0bb8de0f7554
李振宇 <lizhenyu@joyware.com>
2024-03-27 14:58:28 +0800

    修改为powershell脚本

---
+++ depoly.ps1
@@ -0,0 +1,11 @@
+$ErrorActionPreference = "Stop"
+Write-Host "开始打包"
+yarn run build
+cd docs/.vitepress/dist
+Write-Host "准备发布"
+git init
+git add -A
+git commit -m "deploy更新"
+Write-Host "准备上传"
+git push -f git@github.com:happyplum/guideNotes.git main:gh-pages
+Write-Host "上传完毕"
